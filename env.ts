e27292e9053b2de629e709144742ddf8b4c2ab02
MarzanaBegum <marzanaBegum473@gmail.com>
2024-01-03 21:01:58 +0600

    change server url

--- env.ts
+++ env.ts
@@ -1,2 +1,2 @@
 // export const SERVER_URL = 'http://localhost:4000';
-export const SERVER_URL = 'https://prevention-server.up.railway.app';
+export const SERVER_URL = 'https://preventive-pal-server.onrender.com';
